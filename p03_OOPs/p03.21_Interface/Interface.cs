d4feb5751f98bb92b1ad681cf692d835f85e6e5e
shaella <shirouchan38@hotmail.com>
2023-06-14 16:06:54 +0700

    Adding new records: Interface and Enums

---
+++ p03_OOPs/p03.21_Interface/Interface.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace AbstractClassMethods
+{
+	class Program
+	{
+		static void Main()
+		{
+			ITestInterface1 obj = new ImplementationClass();
+			obj.Add(10, 20);
+			
+			ImplementationClass1 obj1 = new ImplementationClass1();
+			obj1.Add(10,20);
+			ImplementationClass2 obj2 = new ImplementationClass2();
+			obj2.Add(10,20);
+			obj2.Sub(100,20);
+
+			ImplementationClass obj4 = new ImplementationClass();
+			obj4.Add(10,20);
+
+			((ITestInterface1)obj4).Sub(100,20);
+
+			ITestInterface1 obj3 = new ImplementationClass();
+			obj3.Add(200,50);
+			obj3.Sub(200,50);
+
+			Console.ReadKey();
+		}
+	}
+
+interface ITestInterface1
+{
+	void Add(int num1, int num2);
+	void Sub(int num1, int num2);
+}
+interface ITestInterface2 : ITestInterface1
+{
+	void Sub(int num1, int num2);
+}
+
+public class ImplementationClass1 : ITestInterface1
+{
+	public void Add(int num1, int num2)
+	{
+		Console.WriteLine($"Sum of {num1} and {num2} is {num1 + num2}");
+	}
+	public void Sub(int num1, int num2)
+        {
+                Console.WriteLine($"Division of {num1} and {num2} is {num1 - num2}");
+	}
+}
+
+public class ImplementationClass2 : ITestInterface2
+{
+        public void Add(int num1, int num2)
+        {
+                Console.WriteLine($"Sum of {num1} and {num2} is {num1 + num2}");
+        }
+	public void Sub(int num1, int num2)
+        {
+                Console.WriteLine($"Division of {num1} and {num2} is {num1 - num2}");
+        }
+}
+
+public class ImplementationClass : ITestInterface1
+{
+        public void Add(int num1, int num2)
+        {
+                Console.WriteLine($"Sum of {num1} and {num2} is {num1 + num2}");
+        }
+	public void Sub(int num1, int num2)
+        {
+                Console.WriteLine($"Division of {num1} and {num2} is {num1 - num2}");
+        }
+}
+}
---
+++ p12_OptionalParameter-IndexersAndEnums/p12.04_Enums/Enums.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Enums
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] EnumValues = (int[])Enum.GetValues(typeof(Gender));
+			Console.WriteLine("Gender Enum Values");
+			foreach (int value in EnumValues)
+			{
+				Console.WriteLine(value);
+			}
+			Console.WriteLine();
+			string[] EnumNames = Enum.GetNames(typeof(Gender));
+			Console.WriteLine("Gender Enum Names");
+			foreach (string Name in EnumNames)
+                        {
+                                Console.WriteLine(Name);
+                        }
+
+			
+			List<Employee> empList = new List<Employee>
+			{
+				new Employee() { Name = "Anurag", Gender = 0 },
+				    new Employee() { Name = "Pranaya", Gender = 1 },
+				    new Employee() { Name = "Priyanka", Gender = 2 },
+				    new Employee() { Name = "Sambit", Gender = 3 },
+			};
+			
+			foreach (var emp in empList)
+			{
+				Console.WriteLine($"Name = {emp.Name} && Gender = {GetGender(emp.Gender)}");
+			}
+
+			Gender gender = (Gender)Season.Winter;
+			Console.WriteLine(gender);
+			Console.ReadLine();
+		}
+
+		public static string GetGender(int gender)
+		{
+			switch (gender)
+			{
+//				case 0:
+//					return "Unknown";
+//				case 1:
+//					return "Male";
+//				case 2:
+//					return "Female";
+//				default:
+//					return "Invalid Data for Gender";
+				case (int)Gender.Unknown:
+                                        return "Unknown";
+                                case (int)Gender.Male:
+                                        return "Male";
+                                case (int)Gender.Female:
+                                        return "Female";
+                                default:
+                                        return "Invalid Data for Gender";
+			}
+		}
+	}
+
+	public class Employee
+	{
+		public string Name { get; set; }
+		public int Gender { get; set;}
+	}
+
+	public enum Gender : int
+	{
+		Unknown = 1,
+		Male = 2,
+		Female = 3
+	}
+
+	public enum Season : int
+	{
+		Winter = 1,
+		Spring = 2,
+		Summer = 3
+	}
+}
+
+

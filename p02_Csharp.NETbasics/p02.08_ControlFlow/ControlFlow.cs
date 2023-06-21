78766d02c58df308b87092614832ad71e33a2f32
shaella <shirouchan38@hotmail.com>
2023-06-21 21:22:47 +0700

    Adding ControlFlow and If-Else

---
+++ p02_Csharp.NETbasics/p02.08_ControlFlow/ControlFlow.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace ControlFlow
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Executing Statement1");
+			Console.WriteLine("Executing Statement2");
+			Console.WriteLine("Executing Statement3");
+			Console.WriteLine("Executing Statement4");
+			Console.WriteLine("Executing Statement5");
+
+			if(10 > 5)
+			{
+				Console.WriteLine("Executing Statement1");
+				Console.WriteLine("Executing Statement2");
+			}
+			else
+			{
+				Console.WriteLine("Executing Statement3");
+				Console.WriteLine("Executing Statement4");
+			}
+			Console.WriteLine("Executing Statement5");
+			
+			for (int i = 0; i < 5; i++)
+			{
+				Console.WriteLine("Welcome to C#.NET");
+			}
+			Console.WriteLine("Some Other Statement");
+
+			Console.WriteLine("Executing Statement1");
+			goto statement4;
+			Console.WriteLine("Executing Statement2");
+			Console.WriteLine("Executing Statement3");
+statement4:
+			Console.WriteLine("Executing Statement4");
+			Console.WriteLine("Executing Statement5");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ p02_Csharp.NETbasics/p02.09_If-ElseStatements/If-ElseStatements.cs
@@ -0,0 +1,102 @@
+﻿using System;
+namespace ControlFlow
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int number;
+			Console.WriteLine("Enter a Number: ");
+			number = Convert.ToInt32(Console.ReadLine());
+			if (number > 10)
+			{
+				Console.WriteLine($"{number} is greater than 10 ");
+				Console.WriteLine("End of if block");
+			}
+			Console.WriteLine("End of Main Method");
+
+			int number2;
+                        Console.WriteLine("Enter a Number: ");
+                        number2 = Convert.ToInt32(Console.ReadLine());
+                        if (number2 > 10)
+                                Console.WriteLine($"{number2} is greater than 10 ");
+                        Console.WriteLine("End of Main Method");
+
+			Console.WriteLine("Enter a Number: ");
+			int number3 = Convert.ToInt32(Console.ReadLine());
+			if (number3 % 2 == 0)
+			{
+				Console.WriteLine($"{number3} is an Even Number");
+			}
+			else
+			{
+				Console.WriteLine($"{number3} is an Odd Number");
+			}
+
+			int number4 = 10;
+			if (number4 == 10)
+			{
+				Console.WriteLine("Hi");
+				Console.WriteLine("Hello");
+			}
+			else
+				Console.WriteLine("Bye");
+			
+			int a = 15, b = 25, c = 10;
+			int LargestNumber = 0;
+			if (a > b)
+			{
+				Console.WriteLine($"Outer IF Block");
+				if (a > c)
+				{
+					Console.WriteLine($"Outer IF - Inner IF Block");
+					LargestNumber = a;
+				}
+				else
+				{
+					Console.WriteLine($"Outer IF - Inner ELSE Block");
+					LargestNumber = c;
+				}
+			}
+			else
+			{
+				Console.WriteLine($"Outer ELSE Block");
+				if (b > c)
+				{
+					Console.WriteLine($"Outer ELSE - Inner IF Block");
+					LargestNumber = b;
+				}
+				else
+				{
+					Console.WriteLine($"Outer ELSE - Inner ELSE Block");
+					LargestNumber = c;
+				}
+			}
+			Console.WriteLine($"The Largest Number is :{LargestNumber}");
+
+			Console.WriteLine($"a={a}, b={b}, c={c}");
+			LargestNumber = (a > b) ? ((a > c)?(a):(c)) : ((b > c) ?(b):(c));
+			Console.WriteLine($"The Largest Number is :{LargestNumber}");
+
+			int i = 20;
+			if (i == 10)
+			{
+				Console.WriteLine("i is 10");
+			}
+			else if (i == 15)
+			{
+				Console.WriteLine("i is 15");
+			}
+			else if (i == 20)
+                        {
+                                Console.WriteLine("i is 20");
+                        }
+			else
+			{
+                                Console.WriteLine("i is not present");
+                        }
+
+			Console.ReadKey();
+		}
+	}
+}

c74b2facb3f0e926a058bb694f62506438d7ad86
shaella <shirouchan38@hotmail.com>
2023-06-25 08:30:58 +0700

    Adding new record: Lambda Expression

---
+++ p05_Events-DelegatesAndLambdaExpression/p05.08_LambdaExpressions/LambdaExpressions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LambdaExpressionDemo
+{
+	public class LambdaExpression
+	{
+		public delegate string GreetingsDelegate(string name);
+		static void Main(string[] args)
+		{
+//			GreetingsDelegate obj = new GreetingsDelegate(LambdaExpression.Greetings);
+//			GreetingsDelegate obj = delegate (string name)
+			GreetingsDelegate obj = (name) =>
+			{
+				return "Hello @" + name + " welcome to Dotnet Tutorials";
+			};
+			string GreetingsMessage = obj.Invoke("Pranaya");
+			Console.WriteLine(GreetingsMessage);
+			Console.ReadKey();
+		}
+
+//		public static string Greetings(string name)
+//		{
+//			return "Hello @" + name + " welcome to Dotnet Tutorials";
+//		}
+	}
+}

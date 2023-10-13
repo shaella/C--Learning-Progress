e5878fa073b5f0baab29d0d91e1fa6504a3c2e68
tanshaella <shaella.tan@gmail.com>
2023-10-13 14:30:23 +0700

    Add Static vs Non-Static Constructors record

---
+++ p03_OOPs/p03.06_StaticVsNon-StaticConstructors/StaticVsNon-StaticContructors.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Constructor
+{
+	public class ConstructorsDemo
+	{
+		public static int x;
+		public int y;
+
+		static ConstructorsDemo()
+		{
+			Console.WriteLine("Static Constructor is Called");
+		}
+
+		public ConstructorsDemo()
+		{
+			Console.WriteLine("Non-Static Constructor is Called");
+		}
+
+//		static void Main(string[] args)
+//		{
+//			Console.WriteLine("Main Method Body..");
+//			Console.ReadKey();
+//		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Main Method Started");
+			
+//			Console.WriteLine(ConstructorsDemo.x);
+
+			ConstructorsDemo obj = new ConstructorsDemo();
+			ConstructorsDemo obj1 = new ConstructorsDemo();
+			ConstructorsDemo obj2 = new ConstructorsDemo();
+			
+			Console.WriteLine("Main Method Completed");
+			Console.ReadKey();
+		}
+	}
+}
